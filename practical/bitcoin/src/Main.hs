8009c6a42deea8223390960897c18cc8f8a9d1a7
SetraR <setrar@valiha.com>
2019-12-24 20:08:22 -0500

    .

--- practical/bitcoin/src/Main.hs
+++ practical/bitcoin/src/Main.hs
@@ -1,5 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import           Network.HTTP.Simple            ( httpBS, getResponseBody )
+import qualified Data.ByteString.Char8         as BS
+
+fetchJSON :: IO BS.ByteString
+fetchJSON = do
+  res <- httpBS "https://api.coindesk.com/v1/bpi/currentprice.json"
+  return (getResponseBody res)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  json <- fetchJSON
+  BS.putStrLn json
+
