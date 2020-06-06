0ac8683b7f9b35fb029f3a1d937f1219938171a1
setrar <setrar@valiha.com>
2020-06-05 21:56:33 -0400

    Update listing17-1.hs

--- get-programming-with-haskell/listing17-1.hs
+++ get-programming-with-haskell/listing17-1.hs
@@ -22,3 +22,5 @@ myAny testFunc = (foldr (||) False) . (map testFunc)
 main = do
    print (myLast [1,2,4])
    print (myAny (\a -> a `mod` 2 == 0) [1,5,8,4,10])
+   print (myMin [5,7,8,3,9,0,14])
+   print (myAll (>=5) [46,57,8,5])
