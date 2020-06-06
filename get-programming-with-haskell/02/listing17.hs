285974d651550915d27ca60547d7fd27251db4ee
setrar <setrar@valiha.com>
2020-06-05 21:41:53 -0400

    Create listing17.hs

---
+++ get-programming-with-haskell/02/listing17.hs
@@ -0,0 +1,24 @@
+-- BR
+
+import Data.List
+
+myLast :: [a] -> a
+myLast = head . reverse
+
+myMin :: Ord a => [a] -> a
+myMin = head . sort
+
+myMax :: Ord a => [a] -> a
+myMax = myLast . sort
+
+myAll :: (a -> Bool) -> [a] -> Bool
+myAll testFunc = (foldr (&&) True) . (map testFunc)
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny testFunc = (foldr (||) False) . (map testFunc)
+
+
+
+main = do
+   print (myLast [1,2,4])
+   print (myAny (\a -> a `mod` 2 == 0) [1,5,8,4,10])
