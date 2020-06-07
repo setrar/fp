ac08b0c013ac68fec41eaba88ecb9870058d1a96
setrar <setrar@valiha.com>
2020-06-07 08:22:56 -0400

    Update composition2.hs

--- get-programming-with-haskell/composition2.hs
+++ get-programming-with-haskell/composition2.hs
@@ -1,14 +1,14 @@
 -- https://www.tutorialspoint.com/haskell/haskell_function_composition.htm
 
 eveno :: Int -> Bool 
-noto  :: Bool -> String 
-
 eveno x = if x `rem` 2 == 0 
-   then True 
-else False 
+          then True 
+          else False 
+
+noto  :: Bool -> String 
 noto x = if x == True 
-   then "This is an even Number" 
-else "This is an ODD number" 
+         then "This is an even Number" 
+         else "This is an ODD number" 
 
 main = do 
    putStrLn "Example of Haskell Function composition" 
