cafd0a202bd64d7d6437c50689f8d0e2389f74ce
SetraR <setrar@valiha.com>
2020-06-11 21:06:52 -0400

    .

---
+++ lyah/14/zipper.hs
@@ -0,0 +1,64 @@
+data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)  
+
+freeTree :: Tree Char  
+freeTree =   
+    Node 'P'  
+        (Node 'O'  
+            (Node 'L'  
+                (Node 'N' Empty Empty)  
+                (Node 'T' Empty Empty)  
+            )  
+            (Node 'Y'  
+                (Node 'S' Empty Empty)  
+                (Node 'A' Empty Empty)  
+            )  
+        )  
+        (Node 'L'  
+            (Node 'W'  
+                (Node 'C' Empty Empty)  
+                (Node 'R' Empty Empty)  
+            )  
+            (Node 'A'  
+                (Node 'A' Empty Empty)  
+                (Node 'C' Empty Empty)  
+            )  
+        )
+
+-- Not so good
+
+changeToP' :: Tree Char -> Tree Char  
+changeToP' (Node x l (Node y (Node _ m n) r)) = Node x l (Node y (Node 'P' m n) r)
+
+-- Replace first node with Z to test my understanding of the above function
+
+changeTop'' :: Tree Char -> Tree Char
+changeTop'' (Node _ l r) = Node 'Z' l r
+
+data Direction = L | R deriving (Show)  
+
+type Directions = [Direction]  
+  
+changeToP :: Directions -> Tree Char -> Tree Char  
+changeToP (L:ds) (Node x l r) = Node x (changeToP ds l) r  
+changeToP (R:ds) (Node x l r) = Node x l (changeToP ds r)  
+changeToP [] (Node _ l r) = Node 'P' l r  
+
+elemAt :: Directions -> Tree a -> a  
+elemAt (L:ds) (Node _ l _) = elemAt ds l  
+elemAt (R:ds) (Node _ _ r) = elemAt ds r  
+elemAt [] (Node x _ _) = x 
+
+-- ghci> let newTree = changeToP [R,L] freeTree  
+-- ghci> elemAt [R,L] newTree  
+-- 'P'  
+
+-- changing the character Node in the Tree
+
+changeNode :: Directions -> Tree Char -> Char -> Tree Char  
+changeNode (L:ds) (Node x l r) n = Node x (changeNode ds l n) r  
+changeNode (R:ds) (Node x l r) n = Node x l (changeNode ds r n)  
+changeNode [] (Node _ l r) n = Node n l r  
+
+-- ghci> let newTree = changeNode [R,L] freeTree 'Z'
+-- ghci> elemAt [R,L] newTree 
+-- 'Z'
