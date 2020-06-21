f6df7e52d09947d1c73e3f07436b2d7a4c1c27a1
SetraR <setrar@valiha.com>
2020-06-21 10:14:32 -0400

    .

---
+++ functional-programming-haskell/1.7/functions.hs
@@ -0,0 +1,3 @@
+f = \x -> x+1  -- lambda function
+-- or
+g x = x+1 -- named function
---
+++ functional-programming-haskell/1.7/generics.hs
@@ -0,0 +1,6 @@
+import Data.Map
+
+set :: Data.Map.Map String Integer 
+set = Data.Map.empty
+
+-- set' = Data.Map.insert "Answer" 42 set
